62af1aa1a895b20a68ae37fb4e71c54c6f7d2099
Dev Agrawal <agrawadv@mail.uc.edu>
2022-08-14 19:45:34 -0400

    moved challenge dto to dtos

--- src/data/dummy.ts
+++ src/data/dummy.ts
@@ -1,32 +1,4 @@
-export interface IChallengeDto {
-  id: string;
-  name: string;
-  summary: string;
-  description: string;
-  prize: number;
-  official: boolean;
-  memberCount: number;
-  group: {
-    id: string;
-    name: string;
-    lang: string[];
-    langPrimary: string[];
-    langAll: boolean;
-    classification: string;
-    subclassification: string;
-    memberColor: string;
-  };
-  created: string;
-  updated: string;
-  noOwner: boolean;
-  taskCount: {
-    total: number;
-    habit: number;
-    daily: number;
-    todo: number;
-    reward: number;
-  };
-}
+import { IChallengeDto } from '@/dtos';
 
 const classes: string[] = [];
 export const processChallenges = (challenges: IChallengeDto[]) => {
--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -1,3 +1,4 @@
 export * from './search';
 export * from './config';
 export * from './enums';
+export * from './incoming';
